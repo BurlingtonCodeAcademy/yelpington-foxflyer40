908d76cae2f2889252ae234df7d97a4fea45b7be
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-03-27 12:20:13 -0400

    final edits for resubmit

--- public/restaurant.js
+++ public/restaurant.js
@@ -1,7 +1,6 @@
 let path = window.location.pathname
 let pathArray = path.split('/')
 let id = pathArray.pop()
-//let restId = document.getElementById('id')
 let name = document.getElementById('name')
 let address = document.getElementById('address')
 let phone = document.getElementById('phone')
@@ -10,6 +9,7 @@ let hours = document.getElementById('hours')
 let notes = document.getElementById('notes')
 let noteContainer = document.getElementById('noteList')
 
+// get restaurant data to populate sidebar details
 async function getRest(id) {
 	let restItem = await fetch(`https://json-server.burlingtoncodeacademy.now.sh/restaurants/${id}`)
 		.then((response) => {
@@ -38,6 +38,7 @@ async function getRest(id) {
 
 getRest(id)
 
+// get  restaurant address and website - convert address to lat lon using nominatum
 async function getLatLon(address, website, name) {
 	let pinLocation = await fetch(`https://nominatim.openstreetmap.org/search/?q=${address}&format=json`)
 		.then((data) => {
@@ -49,6 +50,7 @@ async function getLatLon(address, website, name) {
 			let lon = info.lon
 			let restWeb = website
 
+			// create map and center on lat lon of restaurant - drop pin with link to website
 			let mymap = L.map('map').setView([lat, lon], 16)
 
 			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -58,14 +60,6 @@ async function getLatLon(address, website, name) {
 
 			let thisPin = L.marker([lat, lon]).addTo(mymap).bindPopup('Click to go to ' + name + ' website').openPopup()
 
-			// popup on hover
-			// thisPin.on('mouseover', () => {
-			// 	thisPin.openPopup()
-			// })
-			//When moving mouse off pin, close content
-			// thisPin.on('mouseout', () => {
-			// 	thisPin.closePopup()
-			// })
 			thisPin.on('click', () => {
 				window.open(restWeb);
 			});
